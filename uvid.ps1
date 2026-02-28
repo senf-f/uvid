19761c75389a2213caa6e4e1eac4015f762586d0
Mate Mrse <mate.mrse@gmail.com>
2026-02-28 11:59:54 +0100

    Converted .sh to .ps1 file for use in Windows PowerShell

---
+++ uvid.ps1
@@ -0,0 +1,31 @@
+param(
+    [Parameter(Position = 0)]
+    [string]$TextEntry,
+
+    [string]$s,
+    [string]$a
+)
+
+# Default values
+$sourceText = "( - )"
+$author = "[ - ]"
+
+if (-not $TextEntry) {
+    Write-Host "Usage: uvid `"some text entry`" -s `"source`" -a `"author`""
+    exit 1
+}
+
+if ($s) { $sourceText = "($s)" }
+if ($a) { $author = "[$a]" }
+
+# Create log file if it doesn't exist
+$logFile = "uvid_$(Get-Date -Format 'yyyy').log"
+if (-not (Test-Path $logFile)) { New-Item $logFile -ItemType File | Out-Null }
+
+# Get current timestamp
+$timestamp = Get-Date -Format "dd.MM.yyyy HH:mm"
+
+# Append entry to the log file
+Add-Content -Path $logFile -Value "[$timestamp] $TextEntry $author $sourceText"
+
+Write-Host "Entry added to $logFile"
