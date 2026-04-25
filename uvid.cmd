8e4de0635a62fa81845e77472fa5156f52f19960
Mate Mrse <mate.mrse@gmail.com>
2026-04-25 15:50:21 +0200

    Translate --flags to -Flags in uvid.cmd for PowerShell
    When invoked as `uvid --export` on Windows, the .cmd wrapper now
    translates Bash-style --flags to PowerShell -Flags so both syntaxes
    work from the command line.

--- uvid.cmd
+++ uvid.cmd
@@ -1,2 +1,23 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0uvid.ps1" %*
+setlocal EnableDelayedExpansion
+
+set ARGS=
+for %%A in (%*) do (
+    set "ARG=%%~A"
+    if "!ARG!"=="--help" (set "ARG=-Help"
+    ) else if "!ARG!"=="-h" (set "ARG=-Help"
+    ) else if "!ARG!"=="--list" (set "ARG=-List"
+    ) else if "!ARG!"=="--search" (set "ARG=-Search"
+    ) else if "!ARG!"=="--install" (set "ARG=-Install"
+    ) else if "!ARG!"=="--edit" (set "ARG=-Edit"
+    ) else if "!ARG!"=="--delete" (set "ARG=-Delete"
+    ) else if "!ARG!"=="--export" (set "ARG=-Export"
+    ) else if "!ARG!"=="--author" (set "ARG=-Author"
+    ) else if "!ARG!"=="--year" (set "ARG=-Year"
+    ) else if "!ARG!"=="--from" (set "ARG=-From"
+    ) else if "!ARG!"=="--to" (set "ARG=-To"
+    )
+    set ARGS=!ARGS! "!ARG!"
+)
+
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0uvid.ps1" %ARGS%
